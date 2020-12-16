3bddf05bde678ea5c66f1439db16dba2bb761750
hebertcisco <mrrabbitomg@gmail.com>
2020-12-15 21:38:38 -0300

    importing and making promise library available

---
+++ web/src/lib/Correios/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import rastreio from "./service/correios";
+
+export default async (codigo) => {
+  if (codigo.length !== 13) {
+    return {
+      error: "validation_error",
+      message: "O código de rastreio deve conter 13 caracteres.",
+    };
+  }
+  const result = await rastreio(codigo);
+  if (result.length < 1)
+    return {
+      error: "service_error",
+      message: "O serviço não retornou eventos para este código de rastreio.",
+    };
+  return result;
+};
