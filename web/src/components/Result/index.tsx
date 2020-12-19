54c8dbc075dc4f3009d56c55821210acde109bd9
hebertcisco <mrrabbitomg@gmail.com>
2020-12-19 04:15:13 -0300

    Adicionado componente de resultado

---
+++ web/src/components/Result/index.tsx
@@ -0,0 +1,39 @@
+import { ICode, IRastreio } from "../../utils/types";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+
+const Result: React.FC<ICode> = ({ codigo }) => {
+  const [rastreio, setRastreio] = useState([]);
+
+  useEffect(() => {
+    axios.get(`api/rastreio/v1/${codigo}`).then((response: any) => {
+      setRastreio(response.data);
+    });
+  }, []);
+  return (
+    <div className="container">
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Data/Hora</th>
+            <th>Status</th>
+            <th>Local</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rastreio.map((rastreio: IRastreio) => {
+            return (
+              <tr>
+                <td>{rastreio.dataHora}</td>
+                <td>{rastreio.descricao}</td>
+                <td>{rastreio.cidade}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default Result;
