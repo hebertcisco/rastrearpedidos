fe6266f019d06cdf2f86cd5c3ce7a3dc44836027
hebertcisco <mrrabbitomg@gmail.com>
2020-12-25 00:18:26 -0300

    Adicionadas pequenas melhorias de layout

--- web/src/components/Hero/index.tsx
+++ web/src/components/Hero/index.tsx
@@ -1,3 +1,5 @@
+import "../../styles/css/form-index.css";
+
 import React, { FormEvent, useState } from "react";
 
 import { useRouter } from "next/router";
@@ -33,27 +35,76 @@ const Hero = () => {
   }
   return (
     <div
-      className="hero-wrap js-fullheight"
-      style={{
-        backgroundImage: `url('https://rastrearpedidos.web.app/img/bg_1.jpg')`,
-      }}
-      data-stellar-background-ratio="0.5"
+      className="text-center body html"
+      data-new-gr-c-s-check-loaded="14.990.0"
+      data-gr-ext-installed=""
     >
-      <form className="wrapper" onSubmit={handleRastrear}>
-        <input
-          type="text"
-          className="input"
-          placeholder="Digite seu código de rastreio"
-          value={code}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
-            setCode(event.target.value);
-            return;
-          }}
-        />
-        <div className="searchbtn" onClick={handleRastrear}>
-          <i className="fa fa-search"></i>
+      <div className="container">
+        <div className="text-center d-none d-sm-block">
+          <h3 style={{ paddingBottom: "100px", paddingTop: "50px" }}>
+            <mark>
+              Através deste canal você pode obter informações do rastreio de sua
+              encomenda.{" "}
+            </mark>
+          </h3>
         </div>
-      </form>
+        <div className="text-center d-block d-sm-none">
+          <h6 style={{ paddingBottom: "50px", paddingTop: "25px" }}>
+            <mark>
+              Através deste canal você pode obter informações do rastreio de sua
+              encomenda.{" "}
+            </mark>
+          </h6>
+        </div>
+
+        <div className="row justify-content-md-center">
+          <div className="col-md-auto col">
+            <div className="form-login">
+              <form
+                className="form-signin"
+                onSubmit={handleRastrear}
+                role="form"
+              >
+                <h1 className="h3 mb-3 font-weight-normal text-white">
+                  Digite seu código de Rastreio
+                </h1>
+
+                <p></p>
+                <label htmlFor="cpf" className="sr-only">
+                  Informe seu código de Rastreio
+                </label>
+                <input
+                  type="text"
+                  name="code"
+                  className="form-control"
+                  placeholder="Digite seu código de rastreio"
+                  value={code}
+                  onChange={(
+                    event: React.ChangeEvent<HTMLInputElement>
+                  ): void => {
+                    setCode(event.target.value);
+                    return;
+                  }}
+                  required
+                  autoFocus
+                />
+                <div className="checkbox mb-3"></div>
+                <button
+                  className="btn btn-lg btn-primary btn-block"
+                  type="submit"
+                  id="submit"
+                >
+                  <i className="fa fa-search"></i> Rastrear Pedido
+                </button>
+                <p className="mt-5 mb-3 text-white">
+                  * Informações sobre sua compra podem levar até 72h úteis para
+                  aparecerem no sistema.
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
