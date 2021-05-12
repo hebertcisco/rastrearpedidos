b12155ff5cb72e501c358f8580e7bafb1760f9d5
hebertcisco <mrrabbitomg@gmail.com>
2021-05-12 13:53:09 -0300

    fix seo

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -18,20 +18,35 @@ export default class CustomDocument extends Document<DocumentProps> {
   static async getInitialProps({ renderPage }: DocumentContext) {
     const sheet = new ServerStyleSheet();
 
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
     );
 
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
   }
-
+  setGoogleTags() {
+    return {
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-F610TZ8SNX');
+      `,
+    };
+  }
   public render() {
     const { styleTags } = this.props;
     return (
       <Html>
         <Head>
           {styleTags}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-153085951-1"
+          />
+          {/* We call the function above to inject the contents of the script tag */}
+          <script dangerouslySetInnerHTML={this.setGoogleTags()} />
 
           <></>
         </Head>
